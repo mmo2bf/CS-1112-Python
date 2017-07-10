2e34ba9803af1087095b1dd033f14a63890921e2
Meghan O'Hara <mmo2bf@virginia.edu>
2017-07-09 21:42:49 -0400

    Lab 1

---
+++ LifeCycle.h
@@ -0,0 +1,26 @@
+// Meghan O’Hara mmo2bf 08/30/2016 LifeCycle.h
+#ifndef LIFECYCLE_H
+#define LIFECYCLE_H 
+#include <string>
+using namespace std; 
+
+// ---------------------------------------------------  class definition
+class MyObject {
+public:
+    static int numObjs;
+    MyObject(const char *n = "--default--");      // default constructor
+    MyObject(const MyObject& rhs);                // copy constructor
+    ~MyObject();                                  // destructor
+    string getName() const {
+        return name;
+    }
+    void setName(const string newName) {
+        name = newName;
+    }
+    friend ostream& operator<<(ostream& output, const MyObject& obj);
+private:
+    string name;
+    int id;
+};
+
+#endif
